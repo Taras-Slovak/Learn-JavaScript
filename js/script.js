c246c3ae598f967a79dfbcf6c1e1aa46f830c928
Taras Slovak <slovak.taras@gmail.com>
2020-09-03 00:51:47 +0300

    normal 3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 let numberOfFilms;
 
 do {
-    numberOfFilms = +prompt('Скільки ти фільмів вже подивився?', '');
+    numberOfFilms = prompt('Скільки ти фільмів вже подивився?', '');
 
 } while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms) || numberOfFilms.length > 2);
 
@@ -11,9 +11,12 @@ if (numberOfFilms < 11) {
     alert('Дуже мала кілкість');
 } else if (numberOfFilms < 30) {
     alert('Достаня кількість');
+} else if (numberOfFilms < 50) {
+    alert('Ви кіноман');
 } else {
-    alert('Дуже багато');
+    alert('Помилка');
 }
+
 
 
 let personalMovieDB = {
@@ -35,8 +38,8 @@ for (let i = 0; i < 2; i++){
     } while (a === '' || a === null || a.length > 50);
 
         do {
-        b = +prompt('Яку оцінку поставите йому?', '');
-    } while (b === '' || b === null || isNaN(numberOfFilms) || b.length > 1);
+        b = prompt('Яку оцінку поставите йому?', '');
+    } while (b === '' || b === null || isNaN(b) || b.length > 2);
 
     personalMovieDB.movies[a] = b;
 }
