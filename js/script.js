4865ffc0061dde106cf9a476a5a380ad79357c2a
Taras Slovak <slovak.taras@gmail.com>
2020-09-04 18:18:46 +0300

    function is OK

--- js/script.js
+++ js/script.js
@@ -1,43 +1,35 @@
 'use strict';
 
+
 let numberOfFilms;
 
-do {
-    numberOfFilms = prompt('Скільки ти фільмів вже подивився?', '');
+function start() {
 
-    if (numberOfFilms === '') {
-        alert('Пусто');
-    }
-    if (numberOfFilms === null) {
-        alert('Нажато відміна');
-    }
-    if (isNaN(numberOfFilms)) {
-        alert('Тільки цифри');
-    }
-    if (numberOfFilms >= 1000) {
-        alert('Помилка');
-}
-  
-} while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms) || numberOfFilms.length > 3);
+    do {
+        numberOfFilms = +prompt('Скільки ти фільмів вже подивився?', '');
 
+        if (numberOfFilms === '') {
+            alert('Пусто');
+        }
+        if (numberOfFilms === null) {
+            alert('Нажато відміна');
+        }
+        if (isNaN(numberOfFilms)) {
+            alert('Тільки цифри');
+        }
+        if (numberOfFilms >= 1000) {
+            alert('Помилка');
+        }
 
-
-if (numberOfFilms > 0 && numberOfFilms <= 250 ) {
-    alert('Мала кількість, покзник для начинаючого кіномана');
-}
-if (numberOfFilms > 251 && numberOfFilms <= 500) {
-    alert('В можете вступити в клуб кіноманів');
-}
-if (numberOfFilms > 501 && numberOfFilms <= 750) {
-    alert('Ви гуру серед кіноманів');
-}
-if ( numberOfFilms > 750 && numberOfFilms <= 999) {
-    alert('Це тягне на рекорд Гінеса');
+    } while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms.length > 3);
+    
 }
 
+start();
 
 let personalMovieDB = {
-    count: +numberOfFilms,
+
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -45,48 +37,71 @@ let personalMovieDB = {
 };
 
 
-let a,
-    b;
+function rememberMyFilms() {
 
-for (let i = 0; i < 2; i++) {
+    let a,
+        b;
 
-    do {
-        a = prompt('Один із послідніх фільмів, які Ви подивились?', '');
+    for (let i = 0; i < 2; i++) {
 
-        if (a === '') {
-            alert('Пусто');
-        }
-        if (a === null) {
-            alert('Нажато відміна');
-        }
-        if (a.length > 50 ) {
-            alert('Перевищина кількість знаків');
-        }
-       
+        do {
+            a = prompt('Один із послідніх фільмів, які Ви подивились?', '');
 
-    } while (a === '' || a === null || a.length > 50);
+            if (a === '') {
+                alert('Пусто');
+            }
+            if (a === null) {
+                alert('Нажато відміна');
+            }
+            if (a.length > 50) {
+                alert('Перевищина кількість знаків');
+            }
 
-    do {
-        b = prompt('Яку оцінку поставите йому?', '');
 
-        if (b === '') {
-            alert('Пусто');
-        }
-        if (b === null) {
-            alert('Нажато відміна');
-        }
-        if (isNaN(b)) {
-            alert('Тільки цифри');
-        }
-        if (b > 100) {
-            alert('Рейтинг з 0 до 100');
+        } while (a == '' || a == null || a.length > 50);
+
+        do {
+            b = prompt('Яку оцінку поставите йому?', '');
+
+            if (b === '') {
+                alert('Пусто');
+            }
+            if (b === null) {
+                alert('Нажато відміна');
+            }
+            if (isNaN(b)) {
+                alert('Тільки цифри');
+            }
+            if (b > 100) {
+                alert('Рейтинг з 0 до 100');
+            }
+
+        } while (b == '' || b == null || isNaN(b) || b.length > 2);
+
+        personalMovieDB.movies[a] = b;
+
     }
+}
 
-    } while (b === '' || b === null || isNaN(b) || b.length > 2);
+function detectPersonalLevel() {
 
-    personalMovieDB.movies[a] = b;
+    if (numberOfFilms > 0 && numberOfFilms <= 250) {
+        alert('Мала кількість, покзник для начинаючого кіномана');
+    }
+    if (numberOfFilms > 251 && numberOfFilms <= 500) {
+        alert('В можете вступити в клуб кіноманів');
+    }
+    if (numberOfFilms > 501 && numberOfFilms <= 750) {
+        alert('Ви гуру серед кіноманів');
+    }
+    if (numberOfFilms > 750 && numberOfFilms <= 999) {
+        alert('Це тягне на рекорд Гінеса');
+    }
 }
 
 
-console.log(personalMovieDB);
 
+rememberMyFilms();
+detectPersonalLevel();
+
+console.log(personalMovieDB);
