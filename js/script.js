8641bfd6ef922a0479e5b3a2e02944fbc2beb8ee
Taras Slovak <slovak.taras@gmail.com>
2020-09-11 17:02:57 +0300

    refactoring 1

--- js/script.js
+++ js/script.js
@@ -1,129 +1,134 @@
 'use strict';
 
-let numberOfFilms;
-
-
-
-function start() {
-
-    do {
-        numberOfFilms = prompt('Скільки ти фільмів вже подивився?', '');
-
-        if (numberOfFilms === '') {
-            alert('Пусто');
-        }
-        if (numberOfFilms === null) {
-            alert('Нажато відміна');
-        }
-        if (isNaN(numberOfFilms)) {
-            alert('Тільки цифри');
-        }
-        if (numberOfFilms >= 1000) {
-            alert('Помилка');
-        }
-
-    } while (numberOfFilms ==='' || numberOfFilms === null || isNaN(numberOfFilms) || numberOfFilms.length > 3);
-
-}
-
-// start();
-
-
-function detectPersonalLevel() {
-
-    if (numberOfFilms > 0 && numberOfFilms <= 250) {
-        alert('Мала кількість, покзник для начинаючого кіномана');
-    }
-    if (numberOfFilms > 251 && numberOfFilms <= 500) {
-        alert('В можете вступити в клуб кіноманів');
-    }
-    if (numberOfFilms > 501 && numberOfFilms <= 750) {
-        alert('Ви гуру серед кіноманів');
-    }
-    if (numberOfFilms > 750 && numberOfFilms <= 999) {
-        alert('Це тягне на рекорд Гінеса');
-    }
-}
-
-
-// detectPersonalLevel();
-
-
 let personalMovieDB = {
 
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
+    privat: false,
 
-function rememberMyFilms() {
+    toggleVisibleMyDB: function () {
 
-    let a,
-        b;
+        if (personalMovieDB.privat) {
 
-    for (let i = 0; i < 2; i++) {
+            personalMovieDB.privat = false;
 
-        do {
-            a = prompt('Один із послідніх фільмів, які Ви подивились?', '');
+        } else {
 
-            if (a === '') {
-                alert('Пусто');
-            }
-            if (a === null) {
-                alert('Нажато відміна');
-                continue;
-            }
-            if (a.length > 50) {
-                alert('Перевищина кількість знаків');
-            }
+            personalMovieDB.privat = true;
+        }
 
+    },
 
-        } while (a == '' || a == null || a.length > 50);
+    start: function () {
 
         do {
-            b = prompt('Яку оцінку поставите йому?', '');
+            personalMovieDB.count = prompt('Скільки ти фільмів вже подивився?', '');
 
-            if (b === '') {
+            if (personalMovieDB.count === '') {
                 alert('Пусто');
             }
-            if (b === null) {
+            if (personalMovieDB.count === null) {
                 alert('Нажато відміна');
             }
-            if (isNaN(b)) {
+            if (isNaN(personalMovieDB.count)) {
                 alert('Тільки цифри');
             }
-            if (b > 100) {
-                alert('Рейтинг з 0 до 100');
+            if (personalMovieDB.count >= 1000) {
+                alert('Помилка');
             }
 
-        } while (b ==='' || b === null || isNaN(b) || b.length > 2);
+        } while (personalMovieDB.count === '' || personalMovieDB.count === null || isNaN(personalMovieDB.count) || personalMovieDB.count.length > 3);
 
-        personalMovieDB.movies[a] = b;
+    },
 
+    detectPersonalLevel: function () {
+
+        if (personalMovieDB.count  > 0 && personalMovieDB.count  <= 250) {
+            alert('Мала кількість, покзник для начинаючого кіномана');
+        }
+        if (personalMovieDB.count  > 251 && personalMovieDB.count  <= 500) {
+            alert('В можете вступити в клуб кіноманів');
+        }
+        if (personalMovieDB.count  > 501 && personalMovieDB.count  <= 750) {
+            alert('Ви гуру серед кіноманів');
+        }
+        if (personalMovieDB.count  > 750 && personalMovieDB.count  <= 999) {
+            alert('Це тягне на рекорд Гінеса');
+        }
+    },
+
+    rememberMyFilms: function () {
+
+        let a,
+            b;
+
+        for (let i = 0; i < 2; i++) {
+
+            do {
+                a = prompt('Один із послідніх фільмів, які Ви подивились?', '');
+
+                if (a === '') {
+                    alert('Пусто');
+                }
+                if (a === null) {
+                    alert('Нажато відміна');
+                    continue;
+                }
+                if (a.length > 50) {
+                    alert('Перевищина кількість знаків');
+                }
+
+
+            } while (a == '' || a == null || a.length > 50);
+
+            do {
+                b = prompt('Яку оцінку поставите йому?', '');
+
+                if (b === '') {
+                    alert('Пусто');
+                }
+                if (b === null) {
+                    alert('Нажато відміна');
+                }
+                if (isNaN(b)) {
+                    alert('Тільки цифри');
+                }
+                if (b > 100) {
+                    alert('Рейтинг з 0 до 100');
+                }
+
+            } while (b === '' || b === null || isNaN(b) || b.length > 2);
+
+            personalMovieDB.movies[a] = b;
+
+        }
+    },
+
+    writeYourGeres: function () {
+
+        for (let i = 0; i < 3; i++) {
+
+            personalMovieDB.genres[i] = prompt(`Ваш улуюлений жанр під номером ${i + 1 }`);
+        }
+    },
+ 
+
+    showMyDB: function () {
+        if (!personalMovieDB.privat) {
+            console.log(personalMovieDB);
+        }
     }
-}
 
-// rememberMyFilms();
+};
 
-function writeYourGeres () {
 
-    for (let i = 0; i < 3; i++) {
-        
-        personalMovieDB.genres[i] = prompt(`Ваш улуюлений жанр під номером ${i + 1 }`);
-    }
 
-    
 
-}
+personalMovieDB.toggleVisibleMyDB();
 
-writeYourGeres ();
 
-function showMyDB () {
-    if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
 
-showMyDB();
+personalMovieDB.showMyDB();
+
